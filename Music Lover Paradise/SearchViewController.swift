41c3554c3b6805bcc0ff73df8e2e144922b34b5c
Andres Wang <gibson90730047@icloud.com>
2018-03-23 11:07:51 +0100

    Handle no results, hook tableview & uisearchBar

--- Music Lover Paradise/SearchViewController.swift
+++ Music Lover Paradise/SearchViewController.swift
@@ -9,10 +9,56 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    var searchResults = [SearchResult]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchResults = []
+        if searchBar.text! != "justin bieber" {
+            for i in 0...2 {
+                let searchResult = SearchResult(title: String(format: "Fake Result %d for '%@'", i, searchBar.text!), artistName: "Andres")
+                searchResults.append(searchResult)
+            }
+        }
+        tableView.reloadData()
+    }
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
+
+extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchResults.count == 0 {
+            return 1
+        } else {
+            return searchResults.count
+        }
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)}
+        
+        if searchResults.count == 0 {
+            cell.textLabel!.text = "(Nothing found)"
+            cell.detailTextLabel!.text = ""
+        } else {
+            let searchResult = searchResults[indexPath.row]
+            cell.textLabel!.text = searchResult.title
+            cell.detailTextLabel!.text = searchResult.artistName
+        }
+        
+        return cell
     }
 }
 
