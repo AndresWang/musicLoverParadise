2b230b9daf2b602e76cfa2e04d052ea086f7d29d
Andres Wang <gibson90730047@icloud.com>
2018-03-23 06:46:06 +0100

    Initial Commit

---
+++ Music Lover ParadiseTests/Music_Lover_ParadiseTests.swift
@@ -0,0 +1,36 @@
+//
+//  Music_Lover_ParadiseTests.swift
+//  Music Lover ParadiseTests
+//
+//  Created by Andres Wang on 23/03/2018.
+//  Copyright © 2018 Andres Wang. All rights reserved.
+//
+
+import XCTest
+@testable import Music_Lover_Paradise
+
+class Music_Lover_ParadiseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
