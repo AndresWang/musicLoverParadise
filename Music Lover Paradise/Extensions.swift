30579a27632246ea5a1b1f0afac687a4b5f8305d
Andres Wang <gibson90730047@icloud.com>
2018-03-24 18:11:50 +0100

    Minor fixes

--- Music Lover Paradise/Extensions.swift
+++ Music Lover Paradise/Extensions.swift
@@ -18,7 +18,7 @@ func > (lhs: Result, rhs: Result) -> Bool {
 extension UIViewController {
     func showNetworkError() {
         let alert = UIAlertController(title: NSLocalizedString("Whoops...", comment: "Network error title"), message: NSLocalizedString("There was an error accessing Discogs database. Please try again", comment: "Network error message"), preferredStyle: .alert)
-        let action = UIAlertAction(title: NSLocalizedString("OK", comment: "Confirm"), style: .default, handler: nil)
+        let action = UIAlertAction(title: NSLocalizedString("OK", comment: "Confirm Button"), style: .default, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
     }
@@ -28,9 +28,9 @@ extension URL {
     static func discogs(searchText: String) -> URL {
         let encodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
         let searchTerm = String(format: "https://api.discogs.com/database/search?type=master&title=%@", encodedText)
-        return URL(string: searchTerm + discogsAPIKeySecret)!
+        return URL(string: searchTerm + discogsKeySecret)!
     }
-    static let discogsAPIKeySecret = "&key=ePhrcpQMVtEUrdiOPNgh&secret=DldGkZBXNumyRZfsPMXhjfhfjhSOuWSd"
+    static let discogsKeySecret = "&key=ePhrcpQMVtEUrdiOPNgh&secret=DldGkZBXNumyRZfsPMXhjfhfjhSOuWSd"
 }
 
 extension Data {
