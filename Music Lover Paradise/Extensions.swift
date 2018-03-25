36761e4c50d74fbb5d054e90b97f9e3db47851ff
Andres Wang <gibson90730047@icloud.com>
2018-03-25 14:19:58 +0200

    Minor fixes

--- Music Lover Paradise/Extensions.swift
+++ Music Lover Paradise/Extensions.swift
@@ -82,12 +82,8 @@ extension String {
     func parseDuration() -> TimeInterval {
         guard !self.isEmpty else {return 0}
         var interval:Double = 0
-        
         let parts = self.components(separatedBy: ":")
-        for (index, part) in parts.reversed().enumerated() {
-            interval += (Double(part) ?? 0) * pow(Double(60), Double(index))
-        }
-
+        for (index, part) in parts.reversed().enumerated() {interval += (Double(part) ?? 0) * pow(Double(60), Double(index))}
         return interval
     }
 }
